4a6c5c66a876e8c61ef78626d3ac1a087d84d280
LucianoAAP <78364395+LucianoAAP@users.noreply.github.com>
2021-08-04 23:44:18 -0300

    Adiciona requisito 15
    * deleta crypto
    
    * começa req 9
    
    * continua req 9
    
    * adiciona requisito 15

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router';
 import Header from '../components/Header';
 
 class Feedback extends Component {
+  constructor() {
+    super();
+    this.handlePlayAgain = this.handlePlayAgain.bind(this);
+    this.state = {
+      playAgain: false,
+    };
+  }
+
+  handlePlayAgain() {
+    this.setState({ playAgain: true });
+  }
+
   render() {
+    const { playAgain } = this.state;
     const data = localStorage.getItem('state');
     const player = JSON.parse(data);
     const qtd = player.player.assertions;
     const n3 = 3;
     const feedback = qtd < n3 ? 'Podia ser melhor...' : 'Mandou bem!';
+    if (playAgain) return <Redirect to="/" />;
     return (
       <div>
         <Header />
@@ -20,6 +35,13 @@ class Feedback extends Component {
         <span data-testid="feedback-total-question">
           { `${qtd} Acertos` }
         </span>
+        <button
+          data-testid="btn-play-again"
+          type="button"
+          onClick={ this.handlePlayAgain }
+        >
+          Play again
+        </button>
       </div>
     );
   }
