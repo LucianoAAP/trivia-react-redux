014edd90a3b424d40ea8b528bd5db4db837ec46e
Bruno Augusto <brunaugusto@gmail.com>
2021-08-04 22:47:55 -0300

    Main group 22 requisito 13 (#287)
    * Fix Erros Requisito 2
    
    * Requisito 11
    
    * Requisito 12
    
    * Requisito 13

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -3,11 +3,16 @@ import Header from '../components/Header';
 
 class Feedback extends Component {
   render() {
+    const data = localStorage.getItem('state');
+    const player = JSON.parse(data);
+    const qtd = player.player.assertions;
+    const n3 = 3;
+    const feedback = qtd < n3 ? 'Podia ser melhor...' : 'Mandou bem!';
     return (
       <div>
         <Header />
         <span data-testid="feedback-text">
-          Teste
+          { feedback }
         </span>
       </div>
     );
