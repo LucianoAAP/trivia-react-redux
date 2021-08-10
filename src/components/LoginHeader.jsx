df344e483cc0cf6d735a8780057c144a50922f65
LucianoAAP <78364395+LucianoAAP@users.noreply.github.com>
2021-08-10 16:06:43 -0300

    Começa css
    * deleta crypto
    
    * começa req 9
    
    * continua req 9
    
    * começa css

---
+++ src/components/LoginHeader.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class LoginHeader extends Component {
+  render() {
+    return (
+      <p>
+        Welcome to the
+        <em>Patusos na Lagoa</em>
+        Trivia!
+      </p>
+    );
+  }
+}
+
+export default LoginHeader;
